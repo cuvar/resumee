ad146b4f628065a766158fac4925d6daf24d700d
cuvar <luca-mueller@gmx.net>
2023-02-18 18:34:14 +0100

    feat: general structure

---
+++ src/sections/Footer.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  function onClick() {
+    console.log("hello world");
+  }
+</script>
+
+<footer class="w-full mt-4 mb-4">
+  <div class="flex justify-between text-sm text-zinc-500">
+    <div>February 2023</div>
+    <a
+      href="https://blinpete-resume.vercel.app/"
+      class="hover:underline active:text-zinc-900"
+      target="_blank"
+      rel="noopener noreferrer"
+      >inspired by <span class="italic">blinpete</span></a
+    >
+    <div>Resumee · Luca Müller</div>
+    <a
+      href="https://github.com/cuvar/resumee"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="hover:underline active:text-zinc-900">Source Code</a
+    >
+    <button on:click={onClick} class="underline decoration-dotted"
+      >PDF version</button
+    >
+  </div>
+</footer>
