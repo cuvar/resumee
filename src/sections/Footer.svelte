23b53d760910d98233f17827d8942429e5ecfe75
cuvar <luca-mueller@gmx.net>
2023-02-21 19:57:11 +0100

    feat: responsive footer

--- src/sections/Footer.svelte
+++ src/sections/Footer.svelte
@@ -1,22 +1,24 @@
 <footer class="w-full mt-16">
-  <div class="flex justify-between text-sm text-zinc-500">
-    <div>February 2023</div>
+  <div
+    class="grid grid-cols-1 gap-2 sm:grid-cols-3 md:grid-cols-5 grid-rows-2 md:grid-rows-1 text-sm text-zinc-500"
+  >
+    <div class="text-center">February 2023</div>
     <a
       href="https://blinpete-resume.vercel.app/"
-      class="hover:underline active:text-zinc-900"
+      class="hover:underline active:text-zinc-900 text-center"
       target="_blank"
       rel="noopener noreferrer"
       >inspired by <span class="italic">blinpete</span></a
     >
-    <div>Resumee · Luca Müller</div>
+    <div class="text-center">Resumee · Luca Müller</div>
     <a
       href="https://github.com/cuvar/resumee"
       target="_blank"
       rel="noopener noreferrer"
-      class="hover:underline active:text-zinc-900">Source Code</a
+      class="hover:underline active:text-zinc-900 text-center">Source Code</a
     >
     <a
-      class="underline decoration-dotted"
+      class="underline decoration-dotted text-center"
       href="/luca-mueller-resumee.pdf"
       target="_blank"
       rel="noreferrer noopener">PDF version</a
